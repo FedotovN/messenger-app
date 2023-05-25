d96326ad455e3cb2dde60bf9c16516563dcf18a9
Nikita Fedotov <nikitka.kozarezov@mail.ru>
2023-05-26 01:39:45 +0300

    fix(config): public path for proper hosting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.NODE_ENV === 'production'
+  ? 'messenger-app'
+  : '/'
 })
